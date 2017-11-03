d481a3a8f887c2da191d2ea9ae7a3958de6d5d9e
Miguel Dorta <dortamiguel@gmail.com>
2017-11-03 14:58:03 +0000

    Added Opportunity component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,16 @@
 <template lang='pug'>
 
 #home
+	opportunity
 
 </template>
 
 <script>
+import Opportunity from './opportunity/Opportunity.vue'
+
 export default {
 	name: 'home',
+
+	components: { Opportunity },
 }
 </script>
--- src/components/opportunity/Opportunity.vue
+++ src/components/opportunity/Opportunity.vue
@@ -1,11 +1,14 @@
 <template lang='pug'>
 
 .opportunity
+	span hey
 
 </template>
 
 <script>
 export default {
 	name: 'opportunity',
+
+	props: ['content'],
 }
 </script>
