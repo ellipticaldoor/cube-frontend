16b397594ed8f9cb45f0c1f93a78fc8da7601ab7
Miguel Dorta <dortamiguel@gmail.com>
2017-11-03 17:27:44 +0000

    Added opportunities sorting

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,39 @@
 <template lang='pug'>
 
 #home
+	#sort
+		span sort by
+		select(v-model='sort.selected')
+			option(
+				v-for='value of sort.options'
+				:value='value'
+			) {{ value }}
+
 	#opportunities
 		opportunity(
 			v-for='content of opportunities'
 			v-bind='{ content }'
 			key='content._id'
+			:track-by='content.id'
 		)
 
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions, mapGetters, mapMutations } from 'vuex'
 import Opportunity from './opportunity/Opportunity.vue'
 
 export default {
 	name: 'home',
 
 	components: { Opportunity },
+
+	data() {
+		return {
+			sort: { selected: 'first', options: ['first', 'last'] },
+			sort_by: -1,
+		}
+	},
 
 	computed: {
 		...mapGetters('opportunities', { findOpportunitiesInStore: 'find' }),
@@ -29,10 +45,25 @@ export default {
 
 	methods: {
 		...mapActions('opportunities', { findOpportunities: 'find' }),
+		...mapMutations('opportunities', { clearOpportunities: 'clearAll' }),
+
+		fetch() {
+			this.findOpportunities({
+				query: { $sort: { investors: this.sort_by } },
+			})
+		},
+	},
+
+	watch: {
+		'sort.selected'() {
+			this.sort_by *= -1
+			this.clearOpportunities()
+			this.fetch()
+		},
 	},
 
 	created() {
-		this.findOpportunities()
+		this.fetch()
 	},
 }
 </script>
